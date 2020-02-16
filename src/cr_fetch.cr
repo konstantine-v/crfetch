42aebc05a8aefb6e1b94519f21849fd55c60d0ba
Konstantine <konstantinev@protonmail.com>
2020-02-15 23:12:02 -0600

    updates to the main file

--- src/cr_fetch.cr
+++ src/cr_fetch.cr
@@ -1,22 +1,22 @@
 require "xml"
+require "shell"
 
 module CrFetch
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
-  puts "Print System Info"
+  os = "MacOS" #Change to actually
+  case os
+  when "Linux" || "Windows"
+    puts "tbd"
+  when "MacOS"
+    cpu_sys="sysctl -n machdep.cpu.brand_string"
+    cpu_cores_log="sysctl -n hw.logicalcpu_max"
+    cpu_cores_log="sysctl -n hw.physicalcpu_max"
 
-  proc_file = File.readable?("/proc/") # See if OS contains the procfile to read from there, if so it's Linux, other then BSD or Mac.
-  if (!proc_file)                      # If false
-    sys_data = system "system_profiler -xml SPHardwareDataType"
-    xml = XML.parse(sys_data.to_s)
-    first = xml.first_element_child
-
-    if first
-      first.children.select(&.element?).each do |child| # Select only element children
-        puts typeof(child)                              # XML::Node
-        puts child.name                                 # firstname : String
-        puts child.content                              # Jane : String?
-      end
-    end
+    print "CPU: "
+    puts Shell.run(cpu_sys)
   end
+
+  #puts Shell.run(cpu_sys)
+
 end
